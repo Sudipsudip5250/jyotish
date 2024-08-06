ce5827116479e4098b66ad7c307c4c3a45a99d0c
sudipsudip5250 <sudipsudip5250@gmail.com>
2024-08-06 20:19:43 +0530

    changed

--- main/js/menu.js
+++ main/js/menu.js
@@ -1,11 +1,11 @@
 export function loadmenu() {
   document.getElementById("menu").innerHTML = `
     <ul> 
-        <li><a href="../../home/home.html">Home</a></li>
-        <li><a href="../../about/about.html">About</a></li>
-        <li><a href="../../service/service.html">Services</a></li>
-        <li><a href="../../gallery/gallery.html">Gallery</a></li>
-        <li><a href="../../contact/contact.html">Contact Us</a></li>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="service.html">Services</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
     </ul>
     `;
 
