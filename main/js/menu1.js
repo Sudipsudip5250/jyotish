b0c15abf87f51632e0d1675d4ff43d82edd82e95
sudipsudip5250 <sudipsudip5250@gmail.com>
2024-08-06 20:41:34 +0530

    added

---
+++ main/js/menu1.js
@@ -0,0 +1,16 @@
+export function loadmenu1() {
+    document.getElementById("menu1").innerHTML = `
+      <ul> 
+          <li><a href="../home.html">Home</a></li>
+          <li><a href="../about.html">About</a></li>
+          <li><a href="../service.html">Services</a></li>
+          <li><a href="../gallery.html">Gallery</a></li>
+          <li><a href="../contact.html">Contact Us</a></li>
+      </ul>
+      `;
+  
+    document.getElementById("footer").innerHTML = `
+      <footer>2024 My Website. All rights reserved.</footer>
+      `;
+  }
+  
--- main/main.js
+++ main/main.js
@@ -1,8 +1,12 @@
 import {loadmenu} from './js/menu.js';
+import {loadmenu1} from './js/menu1.js';
 import {istacal} from '../home/istacal/istacal.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     loadmenu();
+});
+document.addEventListener("DOMContentLoaded", function() {
+    loadmenu1();
 });
 document.addEventListener("DOMContentLoaded", function() {
     istacal();
